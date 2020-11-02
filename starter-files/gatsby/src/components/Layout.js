0f4d2a1afce0b44ceb562edcd52d45f04f9f08df
Benjamin Steidl <localartpool@gmail.com>
2020-11-02 16:23:39 +0100

    worked on global styles, layout and nav + logo

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import 'normalize.css';
 import Nav from './Nav';
 import Footer from './Footer';
+import GlobalStyles from '../styles/GlodalStyles';
+import Typography from '../styles/Typography';
+
 
 export default function Layout(props) {
   return(
     <div>
-      <Nav />
-        {props.children}
-      <Footer />
+      <GlobalStyles />
+      <Typography />
+        <Nav />
+          {props.children}
+        <Footer />
     </div>
   )
 }
