472f3ef0e2587acd403febb51f015440abd7eb93
Benjamin Steidl <localartpool@gmail.com>
2020-11-05 11:07:43 +0100

    Worked on loading images and pizza layout

---
+++ starter-files/gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ToppingFilter() {
+  //Get a list of all the toppings
+  //Get a list of all the pizzas with their toppings
+  //Count how many pizzas are in each topping
+  //Loop over the list of toppings and display the topping and the count of pizzas
+  return (
+    <div>
+      <p>Toppings</p>
+    </div>
+  );
+} 
