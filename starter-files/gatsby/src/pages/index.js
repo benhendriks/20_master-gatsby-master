3f383459b27d1161a159095502d2f6855400bfb2
Benjamin Steidl <localartpool@gmail.com>
2020-11-17 11:05:33 +0100

    Worked on custom hook for client side data fetching

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
 
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 function HomePage() {
-  return(
-    <>
-      <p>Hey i'm the home page</p>
-    </>
+  const { slicemasters, hotSlices } = useLatestData();
+  return (
+    <div className="center">
+      <h1>The best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm every single day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotslices={hotSlices} />
+      </div>
+    </div>
   );
 }
 
